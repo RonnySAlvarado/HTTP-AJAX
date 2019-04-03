a9d55c390e0ab1a34e1b2fbfa31d9e2585238a00
Ronny Alvarado <rsalvarado777@gmail.com>
2019-04-02 21:52:20 -0500

    Added a form that will take 3 inputs and submit and make an axios.post request

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,12 +3,31 @@ import { Route } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 import Friends from "./components/Friends";
+import styled from "styled-components";
+
+const FormStyle = styled.div`
+  border: 1px solid black;
+  width: 500px;
+  margin: 0 auto;
+  button {
+    margin: 20px;
+    width: 100px;
+    height: 40px;
+    background: lawngreen;
+    &:hover {
+      background: green;
+    }
+  }
+`;
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      friends: []
+      friends: [],
+      name: "",
+      age: "",
+      email: ""
     };
   }
 
@@ -23,9 +42,65 @@ class App extends Component {
       });
   }
 
+  inputHandler = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  addPost = event => {
+    event.preventDefault();
+    if (!this.state.name || !this.state.age || !this.state.email) {
+      console.log("Error, must fill all 3 input fields");
+    } else {
+      let newFriend = {
+        name: this.state.name,
+        age: this.state.age,
+        email: this.state.email
+      };
+      axios
+        .post("http://localhost:5000/friends", newFriend)
+        .then(res => {
+          console.log(res.data);
+          this.setState({ friends: res.data, name: "", age: "", email: "" });
+        })
+        .catch(err => console.log(err));
+    }
+  };
+
   render() {
     return (
       <div className="App">
+        <FormStyle>
+          <form onSubmit={this.addPost}>
+            <h2>Name:</h2>
+            <input
+              type="text"
+              placeholder="Enter name..."
+              name="name"
+              onChange={this.inputHandler}
+              value={this.state.name}
+            />
+            <h2>Age:</h2>
+            <input
+              type="text"
+              placeholder="Enter age..."
+              name="age"
+              onChange={this.inputHandler}
+              value={this.state.age}
+            />
+            <h2>Email:</h2>
+            <input
+              type="text"
+              placeholder="Enter email..."
+              name="email"
+              onChange={this.inputHandler}
+              value={this.state.email}
+            />
+            <div>
+              <button type="submit">Add Person</button>
+            </div>
+          </form>
+        </FormStyle>
+
         <Route
           path="/"
           render={props => <Friends {...props} friends={this.state.friends} />}
